35bfe614400ecb8651fcf023e8eee0cd14bb68da
Olubiyitaiwo <olubiyipaul01@gmail.com>
2024-11-02 17:49:16 +0100

    my python assignment

---
+++ pythonAassignment/02- 11- 2024/exercise_3.7.py
@@ -0,0 +1,5 @@
+print(f'number', 'square', 'cube')
+for number in range(0,11,1):
+	print(f'',number,      '      ',number*number,      '   ',number*number*number)
+
+	
---
+++ pythonAassignment/02- 11- 2024/exercise_3.8.py
@@ -0,0 +1,3 @@
+for number in range(1,5,1):
+	number = int(input("Enter number: "))
+	print(f'sum:', number + number + number +number, 'average', number/4, 'product', number*4)
