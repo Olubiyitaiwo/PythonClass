d1c4f3e43f496b8c1b13a1356aea56f6e0c94c82
Olubiyitaiwo <olubiyipaul01@gmail.com>
2024-11-04 08:46:18 +0100

    My python Assignment

---
+++ pythonAassignment/02- 11- 2024/countinue.py
@@ -0,0 +1,4 @@
+for number in range(100):
+  if number == 5:
+	countinue
+	print(number, end=' ')
---
+++ pythonAassignment/02- 11- 2024/multiplication_table.py
@@ -0,0 +1,6 @@
+
+
+for number in range(1, 21, 1):
+	for count in range(1, 12, 1):
+		print(number, 'x', count, '=', number*count, end= "\t " )
+	print() 
